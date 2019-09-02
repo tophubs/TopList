c11a7709e5bf092fb8b71bebbed787f04c51af5b
beiping96 <beiping96@gmail.com>
2019-09-02 19:50:32 +0800

    :zap: (add http handle cache)

--- App/Server.go
+++ App/Server.go
@@ -1,37 +1,85 @@
 package main
 
 import (
-	"../Common"
-	"../Config"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
+	"time"
+
+	"../Common"
+	"../Config"
 )
 
+var (
+	typeInfoStorage      map[string][]byte
+	typeStorage          []byte
+	cacheRW              = new(sync.RWMutex)
+	cacheRefreshDuration = time.Duration(10) * time.Minute
+)
+
+func init() {
+	go cacheLoader()
+}
+func cacheLoader() {
+	ticker := time.Tick(cacheRefreshDuration)
+	for {
+		loadCache()
+		<-ticker
+	}
+}
+func loadCache() {
+	// GetType
+	res := Common.MySql{}.GetConn().Select("hotData2", []string{"name", "id"}).QueryAll()
+	typeS := Common.Message{}.Success("获取数据成功", res)
+	// GetTypeInfo
+	typeInfoS := make(map[string][]byte)
+	data := Common.MySql{}.GetConn().ExecSql("SELECT id,str FROM hotData")
+	for _, one := range data {
+		typeInfoS[one["id"]] = []byte(one["str"])
+	}
+	// Replace
+	cacheRW.Lock()
+	defer cacheRW.Unlock()
+	typeStorage = typeS
+	typeInfoStorage = typeInfoS
+}
+
 func GetTypeInfo(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 	err := r.ParseForm()
 	if err != nil {
 		log.Fatal("系统错误" + err.Error())
 	}
 	id := r.Form.Get("id")
-	sql := "select str from hotData where id=" + id
-	data := Common.MySql{}.GetConn().ExecSql(sql)
-	if len(data) == 0 {
-		fmt.Fprintf(w, "%s", `{"Code":1,"Message":"id错误，无该分类数据","Data":[]}`)
-		return
+
+	var rsp []byte
+	cacheRW.RLock()
+	info, ok := typeInfoStorage[id]
+	if ok {
+		rsp = make([]byte, len(info))
+		copy(rsp, info)
+	} else {
+		rsp = []byte(`{"Code":1,"Message":"id错误，无该分类数据","Data":[]}`)
 	}
-	fmt.Fprintf(w, "%s", data[0]["str"])
+	cacheRW.RUnlock()
+	w.Write(rsp)
 }
 
 func GetType(w http.ResponseWriter, r *http.Request) {
-	res := Common.MySql{}.GetConn().Select("hotData2", []string{"name", "id"}).QueryAll()
-	Common.Message{}.Success("获取数据成功", res, w)
+	defer r.Body.Close()
+	cacheRW.RLock()
+	rsp := make([]byte, len(typeStorage))
+	copy(rsp, typeStorage)
+	cacheRW.RUnlock()
+	w.Write(rsp)
 }
 
 func GetConfig(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 	fmt.Fprintf(w, "%s", Config.MySql().Source)
 }
 
--- Common/Message.go
+++ Common/Message.go
@@ -2,9 +2,7 @@ package Common
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
 )
 
 type Message struct {
@@ -13,24 +11,24 @@ type Message struct {
 	Data    interface{}
 }
 
-func (Message Message) Success(message string, data interface{}, w http.ResponseWriter) {
+func (Message Message) Success(message string, data interface{}) []byte {
 	Message.Code = 0
 	Message.Message = message
 	Message.Data = data
-	jsonStr, err := json.Marshal(Message)
+	jsonBytes, err := json.Marshal(Message)
 	if err != nil {
 		log.Fatal("序列号json错误")
 	}
-	fmt.Fprintf(w, "%s", string(jsonStr))
+	return jsonBytes
 }
 
-func (Message Message) Error(message string, data interface{}, w http.ResponseWriter) {
+func (Message Message) Error(message string, data interface{}) []byte {
 	Message.Code = 1001
 	Message.Message = message
 	Message.Data = data
-	jsonStr, err := json.Marshal(Message)
+	jsonBytes, err := json.Marshal(Message)
 	if err != nil {
 		log.Fatal("序列号json错误")
 	}
-	fmt.Fprintf(w, "%s", string(jsonStr))
+	return jsonBytes
 }
